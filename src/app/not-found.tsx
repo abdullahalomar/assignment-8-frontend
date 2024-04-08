3178d18216efe8c83d96a34c167ec06d66bf381f
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-04-08 20:24:33 +0600

    not found page customized

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+import notFound from "@/assets/404.gif";
+import { Box, Container } from "@mui/material";
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404!! Page Not found</h1>
-    </div>
+    <Container>
+      <Box
+        sx={{
+          margin: {
+            lg: "70px 0 0 80px",
+            md: "70px 0 0 80px",
+            sm: "70px 0 0 20px",
+            xs: "70px 0 0 20px",
+          },
+        }}
+      >
+        <Image src={notFound} width={1000} height={1000} alt="not-found" />
+      </Box>
+    </Container>
   );
 };
 
